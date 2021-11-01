1d00d083041aceaec6792d49685311a78d661dc1
malikertgrl <79869051+malikertgrl@users.noreply.github.com>
2021-11-01 17:14:21 +0300

    bootstrap tutorial

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import React from "react"
 import { View } from "react-native"
 import Router from "./src/navigation/Router"
 import { ThemeProvider } from "./src/utils/Theme/ThemeContext"
-import LoginScreen from './src/login/LoginScreen';
+
 
 const App = () => {
   return (
     <ThemeProvider style={{ flex: 1 }}>
-      {/* <Router /> */}
-      <LoginScreen />
+      <Router />
     </ThemeProvider>
   )
 }
--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import Context from "../components/Context";
 import Async_storage from '../components/Async_storage';
 import HomeDetail from '../components/HomeDetail';
-import LoginScreen from '../login/LoginScreen';
 import {
 
     Mytab
@@ -15,34 +14,6 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 const Drawer = createDrawerNavigator();
 
 
-function DrawerNavigate() {
-    return (
-        <Drawer.Navigator initialRouteName="LoginScreen">
-
-            <Drawer.Screen name="HomeDetail" component={HomeDetail} options={{ headerShown: false, title: "FlatList" }} />
-            <Drawer.Group screenOptions={{
-                headerStyle: {
-                    backgroundColor: 'red'
-                },
-                headerTitleAlign: "center",
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                },
-            }}>
-                <Drawer.Screen name="Context" component={Context}
-                    options={{
-                        title: "ChangeTheme",
-
-                    }} />
-                <Drawer.Screen name="Async_storage" component={Async_storage} options={{ title: "AsyncStorage" }} />
-            </Drawer.Group>
-            <Drawer.Screen name="Mytab" component={Mytab} options={{ headerShown: false, }} />
-            <Drawer.Screen name="LoginScreen" component={LoginScreen} />
-
-        </Drawer.Navigator>
-    )
-}
 
 
 
@@ -50,7 +21,30 @@ function DrawerNavigate() {
 function Router() {
     return (
         <NavigationContainer>
-            {DrawerNavigate()}
+            <Drawer.Navigator initialRouteName="LoginScreen">
+
+                <Drawer.Screen name="HomeDetail" component={HomeDetail} options={{ headerShown: false, title: "FlatList" }} />
+                <Drawer.Group screenOptions={{
+                    headerStyle: {
+                        backgroundColor: 'red'
+                    },
+                    headerTitleAlign: "center",
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+                }}>
+                    <Drawer.Screen name="Context" component={Context}
+                        options={{
+                            title: "ChangeTheme",
+
+                        }} />
+                    <Drawer.Screen name="Async_storage" component={Async_storage} options={{ title: "AsyncStorage" }} />
+                </Drawer.Group>
+                <Drawer.Screen name="Mytab" component={Mytab} options={{ headerShown: false, }} />
+
+
+            </Drawer.Navigator>
         </NavigationContainer >
     );
 }
